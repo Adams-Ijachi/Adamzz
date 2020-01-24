658f1438540824ff1c54b367c4ce4a8eb4eda863
Adams-Ijachi <57960719+Adams-Ijachi@users.noreply.github.com>
2020-01-24 02:38:58 +0100

    Update and rename test9.py to decode_number_to_word.py

--- decode_number_to_word.py
+++ decode_number_to_word.py
@@ -4,27 +4,27 @@ alphabets = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h", 9: 
              26: "z"}
 punct = {1: "!", 2: "?", 3: ",", 4: ".", 5: " ", 6: ";", 7: "'", 8: "\'"}
 user  = [18, 12312, 171,763, 98423, 1208, 216,11,500,18,241,0,32,20620,27,10]
-mode = "up"
+mode = "upper"
 remainder = 0
 for numbers in user:
-    if mode != "pun":
+    if mode != "punuctuation":
         remainder = numbers % 27
 
-        if mode == "up":
+        if mode == "upper":
             if remainder == 0:
-                mode = "low"
+                mode = "lower"
                 continue
             print(alphabets[remainder].upper(),end="")
     if mode == "low":
 
         if remainder == 0 :
-                mode = "pun"
+                mode = "punuctuation"
                 continue
         print(alphabets[remainder],end="")
-    if mode == "pun":
+    if mode == "punuctuation":
         remainder = numbers % 9
         if remainder == 0:
-            mode = "up"
+            mode = "upper"
             continue
         print(punct[remainder],end="")
 
