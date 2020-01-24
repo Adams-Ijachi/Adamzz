4b4e5144ce22f4eff8b108210d7b87621fa1a507
Adams-Ijachi <57960719+Adams-Ijachi@users.noreply.github.com>
2020-01-24 02:46:43 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,46 @@
+m= 6
+k =0
+kq =10
+for i in range(m):
+    for j in range(k):
+        print(end=" ")
+    k+=1
+    for l in range(i):
+        print("#",end="")
+    for o in range(kq):
+            print(end=" ")
+    kq-=1
+    for d in range(kq):
+                print(end=" ")
+    kq-=1
+    for t in range(i):
+        print("#",end="")
+
+
+    print("")
+m = 5
+p = 5
+h=m
+q=0
+pp=0
+for i in range(m):
+    for t in range(p):
+        print(end=" ")
+    p-=1
+
+    i = m- i
+    for w in range(i):
+        print("#",end="")
+    for k in range(q):
+        print(end=" ")
+    q+=1
+    for j in range(pp):
+        print(end=" ")
+    pp+=3
+    for k in range(i):
+        print("#",end="")
+
+
+
+
+    print("")
